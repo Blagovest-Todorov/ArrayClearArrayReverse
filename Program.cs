81f1d59243534aa5330fd52465bce414dc4f3a93
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-27 18:20:59 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _13.ArrayReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3 };
+            Array.Reverse(numbers); //changes the indexes of reverse way so it changes the order //
+            //reversevely of the array  source elements
+            //Array.Reverse(numbers);
+            //Array.Reverse(); inverts the array elements upsidedown
+
+            Array.Clear(numbers, 0, numbers.Length-2); //clears array elements and set the default value o;
+            foreach (var item in numbers)   //clears fro  start index till end index specified ! 
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
